1fa3753b66ada5addf02b3a513c405097a590c8a
AshokPatel01 <46564815+AshokPatel01@users.noreply.github.com>
2020-06-24 17:22:43 +0530

    Revert "JSON viewer static page"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from 'src/app/app.component';
-import { JsonEditorComponent } from 'src/app/json-editor/json-editor.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    JsonEditorComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
