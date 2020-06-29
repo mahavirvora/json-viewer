24ccf2618d8a1ab951e495204a95cd789c626bcf
ashok patel <ashok.patel@indecomm.local>
2020-06-29 11:16:51 +0530

    Comment resolved of Design PR

--- src/app/json-editor/json-editor.component.ts
+++ src/app/json-editor/json-editor.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-json-editor',
-  templateUrl: './json-editor.component.html',
-  styleUrls: ['./json-editor.component.css']
+  templateUrl: 'json-editor.component.html',
+  styleUrls: ['json-editor.component.css']
 })
 export class JsonEditorComponent implements OnInit {
 
