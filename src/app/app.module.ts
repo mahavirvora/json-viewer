70dd50ee671796a79bb48d1c8ce3cb2018e567e2
ashok patel <ashok.patel@indecomm.local>
2020-07-09 17:31:59 +0530

    worked on switch case funtionality for tab space

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from 'src/app/app.component';
 import { JsonEditorComponent } from 'src/app/json-editor/json-editor.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     JsonEditorComponent
   ],
   imports: [
-    BrowserModule, FormsModule, NgbModule
+    BrowserModule, FormsModule, NgbModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/json-editor/json-editor.component.ts
+++ src/app/json-editor/json-editor.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-json-editor',
@@ -13,8 +15,11 @@ export class JsonEditorComponent implements OnInit {
 
   public JSON_Source = "";
   public JSON_Target = "";
+  body: any;
+  selectedSpace: string = '';
+  arrBirds: string[];
 
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+  constructor(config: NgbModalConfig, private modalService: NgbModal, private httpService: HttpClient) {
     // customize default values of modals used by this component tree
     config.backdrop = 'static';
     config.keyboard = false;
@@ -28,7 +33,53 @@ export class JsonEditorComponent implements OnInit {
     this.JSON_Target = JSON.stringify(JSON.parse(this.JSON_Source));
   }
 
+  // spaceSelector() {
+  //   this.selectedSpace.controller('langCtrl', function ($scope) {
+  //     const selectKey = $scope.selected;
+  //     switch (selectKey) {
+  //       case '2':
+  //         alert('2');
+  //         break;
+  //       case '3':
+  //         alert('3');
+  //         break;
+  //       case '4':
+  //         alert('4');
+  //         break;
+  //       default:
+  //         alert('1');
+  //     }
+  //   }
+  // };
+
+  oneTab() {
+    this.JSON_Target = JSON.stringify(this.JSON_Source, null, 1);
+  };
+  twoTab() {
+    this.JSON_Target = JSON.stringify(this.JSON_Source, null, 2);
+  };
+  threeTab() {
+    this.JSON_Target = JSON.stringify(this.JSON_Source, null, 3);
+  };
+  fourTab() {
+    this.JSON_Target = JSON.stringify(this.JSON_Source, null, 4);
+  };
+
+  loadData() {
+    this.body.innerHTML = "";
+    this.body.appendChild(document.createTextNode(JSON.stringify(this.JSON_Source, null, 4)));
+  }
+
   ngOnInit(): void {
+    this.httpService.get('./assets/birds.json').subscribe(
+      data => {
+        this.arrBirds = data as string[];	 // FILL THE ARRAY WITH DATA.
+        //  console.log(this.arrBirds[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.message);
+      }
+    );
   }
 
 }
