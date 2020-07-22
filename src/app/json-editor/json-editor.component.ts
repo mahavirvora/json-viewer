7e8422a5770141b79b716a7cead0dda297de2502
ashok patel <ashok.patel@indecomm.local>
2020-07-22 12:06:52 +0530

    Update click function of button

--- src/app/json-editor/json-editor.component.ts
+++ src/app/json-editor/json-editor.component.ts
@@ -12,16 +12,6 @@ export class JsonEditorComponent implements OnInit {
 
   public JSON_Source = "";
   public JSON_Target = "";
-  json: any;
-  html: string;
-
-  constructor(
-    config: NgbModalConfig,
-    private modalService: NgbModal
-  ) {
-    config.backdrop = 'static';
-    config.keyboard = false;
-  }
 
   minifyJSON() {
     try {
@@ -42,19 +32,8 @@ export class JsonEditorComponent implements OnInit {
     console.log(error);
   }
 
-  Process() {
-    // this.json = $id(this.JSON_Source).value;
-    // try {
-    //   if (json == "") json = "\"\"";
-    //   this.json = eval("[" + json + "]");
-    //   // html = ProcessObject(obj[0], 0, 0, false, false, false);
-    //   $id(this.JSON_Target).innerHTML = " ";
-    //   this.JSON_Source = "{\n  \"id\": \"0001\",\n  \"type\": \"donut\",\n  \"name\": \"Cake\",\n  \"ppu\": 0.55,\n  \"batters\":\n    {\n      \"batter\":\n        [\n          { \"id\": \"1001\", \"type\": \"Regular\" },\n          { \"id\": \"1002\", \"type\": \"Chocolate\" },\n          { \"id\": \"1003\", \"type\": \"Blueberry\" },\n          { \"id\": \"1004\", \"type\": \"Devil's Food\" }\n        ]\n    },\n  \"topping\":\n    [\n      { \"id\": \"5001\", \"type\": \"None\" },\n      { \"id\": \"5002\", \"type\": \"Glazed\" },\n      { \"id\": \"5005\", \"type\": \"Sugar\" },\n      { \"id\": \"5007\", \"type\": \"Powdered Sugar\" },\n      { \"id\": \"5006\", \"type\": \"Chocolate with Sprinkles\" },\n      { \"id\": \"5003\", \"type\": \"Chocolate\" },\n      { \"id\": \"5004\", \"type\": \"Maple\" }\n    ]\n}";
-    //   this.JSON_Target = JSON.parse(this.JSON_Source);
-    // } catch (e) {
-    //   alert("JSON is not well formated:\n" + e.message);
-    //   $id(this.JSON_Target).innerHTML = "";
-    // }
+  formatter() {
+    
   }
 
   ngOnInit(): void { }
