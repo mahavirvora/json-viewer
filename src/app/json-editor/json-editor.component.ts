7ab2701de9d5194f680585bf1106afaad339b332
ashok patel <ashok.patel@indecomm.local>
2020-07-23 11:34:38 +0530

    Browse data from local JSON done

--- src/app/json-editor/json-editor.component.ts
+++ src/app/json-editor/json-editor.component.ts
@@ -13,9 +13,7 @@ export class JsonEditorComponent implements OnInit {
 
   public JSON_Source = "";
   public JSON_Target = "";
-
   body: any;
-  arrBirds: string[];
 
   constructor(
     config: NgbModalConfig,
@@ -29,13 +27,48 @@ export class JsonEditorComponent implements OnInit {
     this.modalService.open(content, { centered: true });
   }
 
-  Minify() {
-    this.JSON_Target = JSON.stringify(JSON.parse(this.JSON_Source));
+  minifyJSON() {
+    try {
+      this.JSON_Target = JSON.stringify(JSON.parse(this.JSON_Source));
+    } catch (e) {
+      this.handleError(e);
+    }
+  }
+  beautifyJSON() {
+    try {
+      this.JSON_Target = JSON.stringify(JSON.parse(this.JSON_Source), null, "    ");
+    } catch (e) {
+      this.handleError(e);
+    }
+  }
+  handleError(error: any) {
+    alert("JSON is not well formated.");
+    console.log(error);
   }
 
   loadData() {
     this.body.appendChild(document.createTextNode(JSON.stringify(this.JSON_Source, null, 4)));
   }
+
+  onFileLoad(event) {
+    const f = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = ((theFile) => {
+      return (e) => {
+        try {
+          const json = JSON.parse(e.target.result);
+          const resSTR = JSON.stringify(json);
+          this.JSON_Source = JSON.parse(resSTR);
+          this.JSON_Source = JSON.stringify(JSON.parse(resSTR), null, "    ");
+        } catch (ex) {
+          alert('exception when trying to parse json = ' + ex);
+        }
+      };
+    })(f);
+    reader.readAsText(f);
+  }
+
 
   ngOnInit(): void {
 
