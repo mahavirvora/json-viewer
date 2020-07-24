1a51a5dad94571f41171ba5585483df1ee0548c2
ashok patel <ashok.patel@indecomm.local>
2020-07-24 20:00:26 +0530

    Tab spacing functionality done

--- src/app/json-editor/json-editor.component.ts
+++ src/app/json-editor/json-editor.component.ts
@@ -14,8 +14,6 @@ export class JsonEditorComponent implements OnInit {
   public JSON_Source = "";
   public JSON_Target = "";
   body: any;
-  isValid: boolean;
-  formatting: { color: string; 'background-color': string; };
 
   constructor(
     config: NgbModalConfig,
@@ -52,45 +50,10 @@ export class JsonEditorComponent implements OnInit {
     this.body.appendChild(document.createTextNode(JSON.stringify(this.JSON_Source, null, 4)));
   }
 
-  onFileLoad(event) {
-    const f = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = ((theFile) => {
-      return (e) => {
-        try {
-          const json = JSON.parse(e.target.result);
-          const resSTR = JSON.stringify(json);
-          this.JSON_Source = JSON.parse(resSTR);
-          this.JSON_Source = JSON.stringify(JSON.parse(resSTR), null, "    ");
-        } catch (ex) {
-          alert('exception when trying to parse json = ' + ex);
-        }
-      };
-    })(f);
-    reader.readAsText(f);
+  selectedSpace(event: any) {
+    this.JSON_Target = JSON.stringify(JSON.parse(this.JSON_Source), undefined ,Number(event.target.value));
   }
 
-  validate() {
-    return (e) => {
-      try {
-        this.message = JSON.parse(this.JSON_Source);
-      } catch (e) {
-        this.isValid = false;
-        this.formatting = { color: 'red', 'background-color': '#f2dede' };
-      }
-      this.message = JSON.parse(this.JSON_Source);
-      this.isValid = true;
-      this.formatting = { color: 'green', 'background-color': '#d0e9c6' };
-    };
-  } message(message: any): any {
-    throw new Error("Method not implemented.");
-  }
-
-
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void { }
 
 }
