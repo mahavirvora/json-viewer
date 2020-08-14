78eeebf51586c497870674fe1958de1fd6e0b221
ashok patel <ashok.patel@indecomm.local>
2020-08-14 20:03:58 +0530

    Json viewr code added but response is not recive proper

--- src/app/json-editor/json-editor.component.ts
+++ src/app/json-editor/json-editor.component.ts
@@ -29,6 +29,14 @@ export class JsonEditorComponent implements OnInit {
   }
   rootNode: any;
   sourceJSONObj: any;
+  code_input: string;
+  load_button: HTMLElement;
+  NodeBoolean: string;
+  NodeNumber: string;
+  NodeString: string;
+  NodeNull: string;
+  NodeObject: string;
+  NodeArray: string;
 
   constructor(
     public config: NgbModalConfig,
@@ -37,7 +45,6 @@ export class JsonEditorComponent implements OnInit {
   ) {
     config.backdrop = 'static';
     config.keyboard = false;
-    this.title = 'Error';
   }
 
   ngOnInit(): void { }
@@ -146,8 +153,28 @@ export class JsonEditorComponent implements OnInit {
     }
   }
 
-  jsonViewer() {
-    this.jsonTarget = JSON.stringify(JSON.parse(this.jsonSource), null, '    ');
+  // JSON Viewer //////
+
+    jsonTreeViewer(jsonTree){
+    var treeWrapper = document.getElementById("tree");
+    var tree = jsonTree.create({}, treeWrapper);
+    function expand() {
+      tree.expand();
+    }
+    function collapse() {
+      tree.collapse();
+    }
+
+    /* Load json form */
+
+    this.code_input = this.jsonSource;
+    this.load_button = document.getElementById('load_code_button');
+
+    function load(e) {
+      this.jsonTreeViewer.parse(this.jsonSource.value);
+      this.jsonSource.value = '';
+    }
+    this.load_button.addEventListener('click', load, false);
 
     return {
       parse: function (json_str) {
@@ -159,36 +186,422 @@ export class JsonEditorComponent implements OnInit {
           alert(e);
         }
 
-        this.tree.loadData(temp);
+        tree.loadData(temp);
+      }
+    }
+  }
+  
+  jsonTree() {
+
+    console.log(this.jsonTreeViewer);
+
+    var utils = {
+
+      getClass: function (val) {
+        return Object.prototype.toString.call(val);
+      },
+
+      getType: function (val) {
+        if (val === null) {
+          return 'null';
+        }
+
+        switch (typeof val) {
+          case 'number':
+            return 'number';
+
+          case 'string':
+            return 'string';
+
+          case 'boolean':
+            return 'boolean';
+        }
+
+        switch (utils.getClass(val)) {
+          case '[object Array]':
+            return 'array';
+
+          case '[object Object]':
+            return 'object';
+        }
+
+        throw new Error('Bad type: ' + utils.getClass(val));
+      },
+
+      forEachNode: function (obj, func) {
+        var type = utils.getType(obj),
+          isLast;
+
+        switch (type) {
+          case 'array':
+            isLast = obj.length - 1;
+
+            obj.forEach(function (item, i) {
+              func(i, item, i === isLast);
+            });
+
+            break;
+
+          case 'object':
+            var keys = Object.keys(obj).sort();
+
+            isLast = keys.length - 1;
+
+            keys.forEach(function (item, i) {
+              func(item, obj[item], i === isLast);
+            });
+
+            break;
+        }
+
+      },
+
+      inherits: (function () {
+        var F = function () { };
+
+        return function (Child, Parent) {
+          F.prototype = Parent.prototype;
+          Child.prototype = new F();
+          Child.prototype.constructor = Child;
+        };
+      })(),
+
+      isValidRoot: function (jsonObj) {
+        switch (utils.getType(jsonObj)) {
+          case 'object':
+          case 'array':
+            return true;
+          default:
+            return false;
+        }
+      },
+
+      extend: function (targetObj, sourceObj) {
+        for (var prop in sourceObj) {
+          if (sourceObj.hasOwnProperty(prop)) {
+            targetObj[prop] = sourceObj[prop];
+          }
+        }
       }
     };
-  }
-  expand() {
-    this.tree.expand();
-  }
-  collapse() {
-    this.tree.collapse();
-  }
 
-  Tree(jsonObj) {
-    this.rootNode = null;
-    this.sourceJSONObj = jsonObj;
-    this.loadData(jsonObj);
+    function Node(label, val, isLast) {
+      var nodeType = utils.getType(val);
 
+      if (nodeType in Node.CONSTRUCTORS) {
+        return new Node.CONSTRUCTORS[nodeType](label, val, isLast);
+      } else {
+        throw new Error('Bad type: ' + utils.getClass(val));
+      }
+    }
 
-    Tree(jsonObj); prototype = {
+    Node.CONSTRUCTORS = {
+      'boolean': NodeBoolean,
+      'number': NodeNumber,
+      'string': NodeString,
+      'null': NodeNull,
+      'object': NodeObject,
+      'array': NodeArray
+    };
 
-      constructor: this.Tree,
+    function _NodeSimple(label, val, isLast) {
+      if (this.constructor === _NodeSimple) {
+        throw new Error('This is abstract class');
+      }
 
-      loadData(jsonObj: any) {
+      var self = this,
+        el = document.createElement('li'),
+        labelEl,
+        template = function (label, val) {
+          var str = '\
+                    <span class="jsontree_label-wrapper">\
+                        <span class="jsontree_label">"' +
+            label +
+            '"</span> : \
+                    </span>\
+                    <span class="jsontree_value-wrapper">\
+                        <span class="jsontree_value jsontree_value_' + self.type + '">' +
+            val +
+            '</span>' +
+            (!isLast ? ',' : '') +
+            '</span>';
+
+          return str;
+        };
+
+      self.label = label;
+      self.isComplex = false;
+
+      el.classList.add('jsontree_node');
+      el.innerHTML = template(label, val);
+
+      self.el = el;
+
+      labelEl = el.querySelector('.jsontree_label');
+
+      labelEl.addEventListener('click', function (e) {
+        if (e.altKey) {
+          self.toggleMarked();
+          return;
+        }
+
+        if (e.shiftKey) {
+          document.getSelection().removeAllRanges();
+          alert(self.getJSONPath());
+          return;
+        }
+      }, false);
+    }
+
+    function NodeBoolean(label, val, isLast) {
+      this.type = "boolean";
+
+      _NodeSimple.call(this, label, val, isLast);
+    }
+    utils.inherits(NodeBoolean, _NodeSimple);
+
+    function NodeNumber(label, val, isLast) {
+      this.type = "number";
+
+      _NodeSimple.call(this, label, val, isLast);
+    }
+    utils.inherits(NodeNumber, _NodeSimple);
+
+    function NodeString(label, val, isLast) {
+      this.type = "string";
+
+      _NodeSimple.call(this, label, '"' + val + '"', isLast);
+    }
+    utils.inherits(NodeString, _NodeSimple);
+
+    function NodeNull(label, val, isLast) {
+      this.type = "null";
+
+      _NodeSimple.call(this, label, val, isLast);
+    }
+    utils.inherits(NodeNull, _NodeSimple);
+
+    function _NodeComplex(label, val, isLast) {
+      if (this.constructor === _NodeComplex) {
+        throw new Error('This is abstract class');
+      }
+
+      var self = this,
+        el = document.createElement('li'),
+        template = function (label, sym) {
+          var comma = (!isLast) ? ',' : '',
+            str = '\
+                        <div class="jsontree_value-wrapper">\
+                            <div class="jsontree_value jsontree_value_' + self.type + '">\
+                                <b>' + sym[0] + '</b>\
+                                <span class="jsontree_show-more">&hellip;</span>\
+                                <ul class="jsontree_child-nodes"></ul>\
+                                <b>' + sym[1] + '</b>' +
+              '</div>' + comma +
+              '</div>';
+
+          if (label !== null) {
+            str = '\
+                        <span class="jsontree_label-wrapper">\
+                            <span class="jsontree_label">' +
+              '<span class="jsontree_expand-button"></span>' +
+              '"' + label +
+              '"</span> : \
+                        </span>' + str;
+          }
+
+          return str;
+        },
+        childNodesUl,
+        labelEl,
+        moreContentEl,
+        childNodes = [];
+
+      self.label = label;
+      self.isComplex = true;
+
+      el.classList.add('jsontree_node');
+      el.classList.add('jsontree_node_complex');
+      el.innerHTML = template(label, self.sym);
+
+      childNodesUl = el.querySelector('.jsontree_child-nodes');
+
+      if (label !== null) {
+        labelEl = el.querySelector('.jsontree_label');
+        moreContentEl = el.querySelector('.jsontree_show-more');
+
+        labelEl.addEventListener('click', function (e) {
+          if (e.altKey) {
+            self.toggleMarked();
+            return;
+          }
+
+          if (e.shiftKey) {
+            document.getSelection().removeAllRanges();
+            alert(self.getJSONPath());
+            return;
+          }
+
+          self.toggle(e.ctrlKey || e.metaKey);
+        }, false);
+
+        moreContentEl.addEventListener('click', function (e) {
+          self.toggle(e.ctrlKey || e.metaKey);
+        }, false);
+
+        self.isRoot = false;
+      } else {
+        self.isRoot = true;
+        self.parent = null;
+
+        el.classList.add('jsontree_node_expanded');
+      }
+
+      self.el = el;
+      self.childNodes = childNodes;
+      self.childNodesUl = childNodesUl;
+
+      utils.forEachNode(val, function (label, node, isLast) {
+        self.addChild(new (Node(label, node, isLast)));
+      });
+
+      self.isEmpty = !Boolean(childNodes.length);
+      if (self.isEmpty) {
+        el.classList.add('jsontree_node_empty');
+      }
+    }
+
+    utils.inherits(_NodeComplex, _NodeSimple);
+
+    utils.extend(_NodeComplex.prototype, {
+      constructor: _NodeComplex,
+
+      addChild: function (child) {
+        this.childNodes.push(child);
+        this.childNodesUl.appendChild(child.el);
+        child.parent = this;
+      },
+
+      expand: function (isRecursive) {
+        if (this.isEmpty) {
+          return;
+        }
+
+        if (!this.isRoot) {
+          this.el.classList.add('jsontree_node_expanded');
+        }
+
+        if (isRecursive) {
+          this.childNodes.forEach(function (item, i) {
+            if (item.isComplex) {
+              item.expand(isRecursive);
+            }
+          });
+        }
+      },
+
+      collapse: function (isRecursive) {
+        if (this.isEmpty) {
+          return;
+        }
+
+        if (!this.isRoot) {
+          this.el.classList.remove('jsontree_node_expanded');
+        }
+
+        if (isRecursive) {
+          this.childNodes.forEach(function (item, i) {
+            if (item.isComplex) {
+              item.collapse(isRecursive);
+            }
+          });
+        }
+      },
+
+      toggle: function (isRecursive) {
+        if (this.isEmpty) {
+          return;
+        }
+
+        this.el.classList.toggle('jsontree_node_expanded');
+
+        if (isRecursive) {
+          var isExpanded = this.el.classList.contains('jsontree_node_expanded');
+
+          this.childNodes.forEach(function (item, i) {
+            if (item.isComplex) {
+              item[isExpanded ? 'expand' : 'collapse'](isRecursive);
+            }
+          });
+        }
+      },
+
+      findChildren: function (matcher, handler, isRecursive) {
+        if (this.isEmpty) {
+          return;
+        }
+
+        this.childNodes.forEach(function (item, i) {
+          if (matcher(item)) {
+            handler(item);
+          }
+
+          if (item.isComplex && isRecursive) {
+            item.findChildren(matcher, handler, isRecursive);
+          }
+        });
+      }
+    });
+
+    function NodeObject(label, val, isLast) {
+      this.sym = ['{', '}'];
+      this.type = "object";
+
+      _NodeComplex.call(this, label, val, isLast);
+    }
+    utils.inherits(NodeObject, _NodeComplex);
+
+    function NodeArray(label, val, isLast) {
+      this.sym = ['[', ']'];
+      this.type = "array";
+
+      _NodeComplex.call(this, label, val, isLast);
+    }
+    utils.inherits(NodeArray, _NodeComplex);
+
+    function Tree(jsonObj, domEl) {
+      this.wrapper = document.createElement('ul');
+      this.wrapper.className = 'jsontree_tree clearfix';
+
+      this.rootNode = null;
+
+      this.sourceJSONObj = jsonObj;
+
+      this.loadData(jsonObj);
+      this.appendTo(domEl);
+    }
+
+    Tree.prototype = {
+      constructor: Tree,
+
+      loadData: function (jsonObj) {
+        if (!utils.isValidRoot(jsonObj)) {
+          alert('The root should be an object or an array');
+          return;
+        }
+
         this.sourceJSONObj = jsonObj;
 
-        this.rootNode = new Node(null, jsonObj, 'last');
+        this.rootNode = new (Node(null, jsonObj, 'last'));
         this.wrapper.innerHTML = '';
         this.wrapper.appendChild(this.rootNode.el);
       },
 
-      expand(filterFunc: any): void {
+      appendTo: function (domEl) {
+        domEl.appendChild(this.wrapper);
+      },
+
+      expand: function (filterFunc) {
         if (this.rootNode.isComplex) {
           if (typeof filterFunc == 'function') {
             this.rootNode.childNodes.forEach(function (item, i) {
@@ -202,17 +615,21 @@ export class JsonEditorComponent implements OnInit {
         }
       },
 
-      collapse(): void {
+      collapse: function () {
         if (typeof this.rootNode.collapse === 'function') {
           this.rootNode.collapse('recursive');
         }
+      },
+    };
+
+    return {
+      create: function (jsonObj, domEl) {
+        return new Tree(jsonObj, domEl);
       }
-    }
-    return() => {
-      (jsonObj) => {
-        return new this.Tree(jsonObj);
-      }
-    }
+    };
   }
+
+
+
 
 }
